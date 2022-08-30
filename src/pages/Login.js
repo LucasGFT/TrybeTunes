7c216b40c8536ee10db304ad0875911765365e2f
Lucas Gomes Filgueiras Tavares <lucasgft200303@gmail.com>
2022-08-30 19:50:03 -0300

    Segundo Requisito

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,71 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Carregando from '../components/Carregando';
 
 class Login extends React.Component {
+  state = {
+    nome: '',
+    isDisableButton: true,
+    carregamento: false,
+    apertoButton: false,
+  };
+
+  haveChange = (event) => {
+    const { target } = event;
+    const { value } = target;
+    this.setState({
+      nome: value,
+    }, () => {
+      const { nome } = this.state;
+      if (nome.length > 2) {
+        this.setState({ isDisableButton: false });
+      } else { this.setState({ isDisableButton: true }); }
+    });
+  };
+
+  haveClickButton = async () => {
+    const { nome, carregamento, apertoButton } = this.state;
+    console.log(carregamento);
+    console.log(apertoButton);
+    console.log(nome);
+    this.setState({ carregamento: true });
+    await createUser({ name: nome });
+    this.setState({ carregamento: false }, () => {
+      this.setState({ apertoButton: true });
+    });
+  };
+
   render() {
-    return <div data-testid="page-login">Login</div>;
+    const { nome, isDisableButton, carregamento, apertoButton } = this.state;
+    return (
+      <div data-testid="page-login">
+        { carregamento === true ? (<Carregando />) : (
+          <form>
+            <label htmlFor="input-nome">
+              Nome:
+              <input
+                data-testid="login-name-input"
+                id="input-nome"
+                onChange={ this.haveChange }
+                value={ nome }
+              />
+              <button
+                type="button"
+                data-testid="login-submit-button"
+                disabled={ isDisableButton }
+                onClick={ this.haveClickButton }
+              >
+                Entrar
+              </button>
+            </label>
+          </form>
+        )}
+        { carregamento === false && apertoButton === true ? (
+          <Redirect to="/search" />
+        ) : null}
+      </div>
+    );
   }
 }
 
